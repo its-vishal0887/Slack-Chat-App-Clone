d9a3849a1446abe7db1982772183199acb80d4be
Vishal <its-vishal0887@users.noreply.github.com>
2026-09-19 09:15:36 +0530

    Added crud repo

--- repositories/crudRepo.js
+++ repositories/crudRepo.js
@@ -1,24 +1,24 @@
-export default function crudRepo(schema){
+export default function crudRepo(model){
   return {
-    model:schema,
-    create:async(data)=>{
-      const newDoc = await this.model.create(data);
+
+    create:async function(data){
+      const newDoc = await model.create(data);
       return newDoc;
     },
-    getAll: async ()=>{
-      const allDocs = await this.model.find();
+    getAll: async function(){
+      const allDocs = await model.find();
       return allDocs;
     },
-    getById:async (id) =>{
-      const doc = await this.model.findById(id);
+    getById:async function(id){
+      const doc = await model.findById(id);
       return doc;
     },
-    delete:async(id) =>{
-      const res = await this.model.findByIdAndDelete(id);
+    delete:async function(id){
+      const res = await model.findByIdAndDelete(id);
       return res;
     },
-    update:async(id, data) =>{
-      const updatedDoc = await this.model.findByIdAndUpdate(id, data, {new:true});
+    update:async function(id, data){
+      const updatedDoc = await model.findByIdAndUpdate(id, data, {new:true});
       return updatedDoc;
     }
   }
--- repositories/userRepo.js
+++ repositories/userRepo.js
@@ -1,41 +1,53 @@
 import User from "../schema/users";
 import crudRepo from "./crudRepo";
 
-export const getUserByEmail = async(email) =>{
-  const user = await User.findOne({email});
-  return user;
-};
+const userRepo = {
+  ...crudRepo(User),
+  getByEmail:async function (email) {
+    const user = await User.findOne({email});
+    return user;
+  },
+  getByUsername:async function (username) {
+    const user = await User.findOne({username});
+    return user;
+  }
+}
 
-export const getUserByName = async(name) =>{
-  const user = await User.findOne({username:name});
-  return user;
-};
-
-// export const createUser = async(user) =>{
-//   const newUser = await User.create(user);
-//   return newUser;
-// };
-
-// export const getUsers  = async() => {
-//   const users = await User.find();
-//   return users;
-// };
-
-// export const getUserById = async(id) =>{
-//   const user = User.findById(id);
+// export const getUserByEmail = async(email) =>{
+//   const user = await User.findOne({email});
 //   return user;
 // };
 
-// export const deleteUser = async(id) => {
-//   const user = await User.findByIdAndDelete(id);
-//   return user
-// };
-
-// export const updateUser = async(id, user) => {
-//   const user = await User.findByIdAndUpdate(id, user, {new:true});
+// export const getUserByName = async(name) =>{
+//   const user = await User.findOne({username:name});
 //   return user;
 // };
 
-const crudMethodes = crudRepo(User);
+// // export const createUser = async(user) =>{
+// //   const newUser = await User.create(user);
+// //   return newUser;
+// // };
 
-export default crudMethodes;
+// // export const getUsers  = async() => {
+// //   const users = await User.find();
+// //   return users;
+// // };
+
+// // export const getUserById = async(id) =>{
+// //   const user = User.findById(id);
+// //   return user;
+// // };
+
+// // export const deleteUser = async(id) => {
+// //   const user = await User.findByIdAndDelete(id);
+// //   return user
+// // };
+
+// // export const updateUser = async(id, user) => {
+// //   const user = await User.findByIdAndUpdate(id, user, {new:true});
+// //   return user;
+// // };
+
+// const crudMethodes = crudRepo(User);
+
+export default userRepo;
